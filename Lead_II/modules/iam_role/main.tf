fecf539f7d57c9f612b3029f74b5d9dd2fb41b52
NeppolieanJoseph <44391067+NeppolieanJoseph@users.noreply.github.com>
2024-08-19 13:06:51 +0530

    Update main.tf

--- Lead_II/modules/iam_role/main.tf
+++ Lead_II/modules/iam_role/main.tf
@@ -15,7 +15,7 @@ resource "aws_iam_role" "glue_role" {
   })
 }
 
-/*resource "aws_iam_policy" "glue_policy" {
+resource "aws_iam_policy" "glue_policy" {
   name = "${var.role_name}-policy"
 
   policy = jsonencode({
@@ -55,7 +55,7 @@ resource "aws_iam_role" "glue_role" {
 resource "aws_iam_role_policy_attachment" "glue_role_attach" {
   role       = aws_iam_role.glue_role.name
   policy_arn = aws_iam_policy.glue_policy.arn
-}*/
+}
 
 resource "aws_iam_role_policy_attachment" "glue_role_attach" {
   role      = aws_iam_role.glue_role.name
